3dc78102bf7f1e1db54e65723b0a72a8a86861ea
aws-jenny <jenny.ayala@awsys-i.com>
2019-07-30 10:29:06 +0800

    implemented counter cache for users per bu

--- db/migrate/20190729011455_create_business_units.rb
+++ db/migrate/20190729011455_create_business_units.rb
@@ -2,6 +2,7 @@ class CreateBusinessUnits < ActiveRecord::Migration[5.1]
   def change
     create_table :business_units do |t|
       t.string :name, null: false, default: ""
+      t.integer :users_count, null: false, default: 0
       t.timestamps
     end
   end

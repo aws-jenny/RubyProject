59d43883965cc8ee1e709d8a470ea625ecae1005
aws-jenny <jenny.ayala@awsys-i.com>
2019-07-31 16:22:06 +0800

    dashboard update using google charts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,12 +5,14 @@ class PagesController < ApplicationController
 		# if params.has_key?(:status) && params[:status] != 'All'
 		# 	@user_certificates = current_user.user_certificate_records.where("status = ?", params[:status])
 		# end
-
+		@onWish = current_user.user_certificate_records.where(status: 'Wished')
 		@ongoing = current_user.user_certificate_records.where(status: 'Ongoing')
 		@taken = current_user.user_certificate_records.where(status: 'Taken')
 		@passed = current_user.user_certificate_records.where(status: 'Passed')
 
 		@passed_certs = UserCertificateRecord.where(status: 'Passed')
+		@top_certs = UserCertificateRecord.joins(:certificate).where(status: 'Passed').group(:name).order('count_id desc').limit(5).count('id')
+		@recommendations = Recommendation.all
 
 		 # select certificate_id, count(certificate_id) as "certified users" from user_certificate_records where status = 'Ongoing' 
  # 	group by certificate_id order by count(certificate_id) desc;

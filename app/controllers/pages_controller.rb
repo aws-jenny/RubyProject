2fb0638dd2d8daa843d6fba84bbe7d00a0ef3aba
aws-jenny <jenny.ayala@awsys-i.com>
2019-08-01 14:26:30 +0800

    on wishlist category in dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
 		# if params.has_key?(:status) && params[:status] != 'All'
 		# 	@user_certificates = current_user.user_certificate_records.where("status = ?", params[:status])
 		# end
-
+		@onWish = current_user.user_certificate_records.where(status: 'Wished')
 		@ongoing = current_user.user_certificate_records.where(status: 'Ongoing')
 		@taken = current_user.user_certificate_records.where(status: 'Taken')
 		@passed = current_user.user_certificate_records.where(status: 'Passed')

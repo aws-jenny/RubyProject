03fc386c04dee69816c974f2f3523809bac9f1f2
aws-jenny <jenny.ayala@awsys-i.com>
2019-08-01 12:37:17 +0800

    dashboard query done, targets ongoing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,30 +10,35 @@ class PagesController < ApplicationController
 		@taken = current_user.user_certificate_records.where(status: 'Taken')
 		@passed = current_user.user_certificate_records.where(status: 'Passed')
 
-		@passed_certs = UserCertificateRecord.where(status: 'Passed')
+		#@passed_certs = UserCertificateRecord.joins(:certificate).where(status: 'Passed')
 		@top_certs = UserCertificateRecord.joins(:certificate).where(status: 'Passed').group(:name).order('count_id desc').
 					limit(5).count('id')
 		@recommendations = Recommendation.all
-		@recommended_and_passed = []
 
-		@recommendations.each do |recommended|
+		@user_ids = @recommendations.map{ |x| x.user_id}
+		@cert_ids = @recommendations.map{ |x| x.certificate_id}
+
+		@recommended_and_passed = UserCertificateRecord.joins(:certificate).where(status: 'Passed', user_id: @user_ids,
+			certificate_id: @cert_ids).group(:name).count('id')
+		
+		#@recommended_and_passed = []
+
+		#@recommendations.each do |recommended|
 
 			#@record = (UserCertificateRecord.where(user_id: recommended.user_id, certificate_id: recommended.certificate_id, status: 'Passed'))
 				
-				@passed_certs.each do |record|
-					if(record.user_id == recommended.user_id && record.certificate_id == recommended.certificate_id)
-						@recommended_and_passed.push(record)
-					end
-				end
+				# @passed_certs.each do |record|
+				# 	if(record.user_id == recommended.user_id && record.certificate_id == recommended.certificate_id)
+				# 		@recommended_and_passed.push(record)
+				# 	end
+				# end
 
 			# unless @record.blank?
 			# 	@recommended_and_passed.push(@record)
 			# end
-		end
+		# end
 
-		puts @recommended_and_passed.size
-		puts @recommended_and_passed
-
+		# puts @recommended_and_passed.size
 		 # select certificate_id, count(certificate_id) as "certified users" from user_certificate_records where status = 'Ongoing' 
  # 	group by certificate_id order by count(certificate_id) desc;
 	end
